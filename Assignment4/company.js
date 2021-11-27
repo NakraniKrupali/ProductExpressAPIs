7c67485a4cdd3e3a687e349ea20ea1f89822b6e0
NakraniKrupali0 <92012510+NakraniKrupali@users.noreply.github.com>
2021-11-27 18:20:58 +0000

    done

---
+++ Assignment4/company.js
@@ -0,0 +1,11 @@
+const company = [
+    {
+        companyId: 1,
+        name: "apple",
+        productId : 1,
+    }
+
+];
+
+
+module.exports = company;
---
+++ Assignment4/product.js
@@ -0,0 +1,14 @@
+const product = [
+    {
+        productId : 1,
+        title : "DSLR", 
+        price : 12000,
+        category  : "camera",
+        companyId : 1, 
+        sellerId :1,
+    }
+
+];
+
+
+module.exports = product;
